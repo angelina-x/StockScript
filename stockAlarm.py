68a8adc549479600d6a8a6b2dbc136e6d6764dee
angelina <angelina.x@qq.com>
2021-12-11 11:04:53 +0800

    configuration

--- stockAlarm.py
+++ stockAlarm.py
@@ -5,11 +5,14 @@ class ReadConfigFile(object):
     def read_config(self):
         conn = ConfigParser()
         file_path = os.path.join(os.path.abspath('.'),'configuration.ini') #os.path.join(os.getcwd(),'configuration.ini')
+        file_path1= os.path.join(os.getcwd(),'configuration.ini')
         print('当前路径'+os.path.abspath('.'))
+        print("file_path:"+file_path)
+        print("file_path1:"+file_path1)
         if not os.path.exists(file_path):
             raise FileNotFoundError("文件不存在")
 
-        conn.read(file_path)
+        conn.read(file_path,encoding='utf-8')
         mail_host=conn.get('email','mail_host')
         mail_user=conn.get('email','mail_user')
         mail_pass=conn.get('email','mail_pass')
