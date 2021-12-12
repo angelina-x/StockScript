c07fba2c481633a827070b704f68b2942cdd60ac
angelina <angelina.x@qq.com>
2021-12-12 13:35:29 +0800

    fetch data

--- stockAlarm.py
+++ stockAlarm.py
@@ -1,5 +1,10 @@
 from configparser import ConfigParser
 import os
+import requests
+import chardet
+import re
+from lxml import etree
+import csv
 
 class ReadConfigFile(object):
     def read_config(self):
@@ -22,3 +27,17 @@ class ReadConfigFile(object):
         
 rc = ReadConfigFile()
 print(rc.read_config())
+
+
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+    #在爬虫里面如果出现了Referer最好也粘上去，因为有时候服务器会根据Referer来判断请求时由浏览器还是爬虫发出的
+    'Referer':'https://www.douban.com/'
+}
+#url = "https://data.eastmoney.com/zjlx/detail.html"
+url = "https://data.eastmoney.com/zjlx/000008.html"
+response = requests.get(url,headers=headers)#发起请求得到响应
+response.encoding = "utf-8"
+text = response.text#返回一个经过解码的字符串
+print(text)
